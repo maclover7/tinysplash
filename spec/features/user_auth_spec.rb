d4b98012973baca3df6ec131f8ef153bdaca0092
Jason Ramirez <jason@thoughtbot.com>
2015-02-18 11:49:23 -0500

    Clean up generated files

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -3,15 +3,18 @@ require "rails_helper"
 feature "User signs up" do
   scenario "successfully" do
     visit root_path
-    click_on "Sign up"
+    click_on t("links.sign_up")
 
-    fill_in "Email", with: "user@example.com"
-    fill_in "* Password", with: "password", exact: true
-    fill_in "Password confirmation", with: "password"
-    click_on "Sign up"
+    fill_in t("simple_form.labels.defaults.email"), with: "user@example.com"
+    fill_in("user_password", with: "password")
+    fill_in(
+      t("simple_form.labels.defaults.password_confirmation"),
+      with: "password"
+    )
+    click_on t("buttons.sign_up")
 
-    expect(page).to have_content "You have signed up successfully."
-    expect(page).to have_content "Sign out"
+    expect(page).to have_content t("devise.registrations.signed_up")
+    expect(page).to have_content t("links.sign_out")
   end
 end
 
@@ -20,14 +23,14 @@ feature "User signs in" do
     create(:user, email: "user@example.com", password: "password")
 
     visit root_path
-    click_on "Sign in"
+    click_on t("links.sign_in")
 
-    fill_in "Email", with: "user@example.com"
-    fill_in "Password", with: "password"
-    click_on "Log in"
+    fill_in t("simple_form.labels.defaults.email"), with: "user@example.com"
+    fill_in t("simple_form.labels.defaults.password"), with: "password"
+    click_on t("buttons.sign_in")
 
-    expect(page).to have_content "Signed in successfully."
-    expect(page).to have_content "Sign out"
+    expect(page).to have_content t("devise.sessions.signed_in")
+    expect(page).to have_content t("links.sign_out")
   end
 end
 
@@ -37,9 +40,9 @@ feature "User signs out" do
     sign_in(email: "user@example.com", password: "password")
 
     visit root_path
-    click_on "Sign out"
+    click_on t("links.sign_out")
 
-    expect(page).to have_content "Signed out successfully."
-    expect(page).to have_content "Sign in"
+    expect(page).to have_content t("devise.sessions.signed_out")
+    expect(page).to have_content t("links.sign_in")
   end
 end
--- spec/support/features/user_auth.rb
+++ spec/support/features/user_auth.rb
@@ -1,10 +1,10 @@
 module Features
-  def sign_in(email: , password:)
+  def sign_in(email:, password:)
     visit new_user_session_path
 
-    fill_in "Email", with: email
-    fill_in "Password", with: password
+    fill_in t("simple_form.labels.defaults.email"), with: email
+    fill_in t("simple_form.labels.defaults.password"), with: password
 
-    click_on "Log in"
+    click_on t("buttons.sign_in")
   end
 end
