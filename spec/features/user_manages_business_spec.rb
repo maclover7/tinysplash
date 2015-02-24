0aa4b58ccd13995b383c0a05abe6025eae6da99f
Avi Gilligan <avi@thoughtbot.com>
2015-02-24 09:55:45 -0500

    Style business pages and nav

--- spec/features/user_manages_business_spec.rb
+++ spec/features/user_manages_business_spec.rb
@@ -7,7 +7,10 @@ feature "User creates a business" do
     visit root_path
     click_on t("links.new_business")
     fill_in t("simple_form.labels.defaults.name"), with: "Example Company"
-    fill_in t("simple_form.labels.defaults.mission"), with: "Example mission statement"
+    fill_in(
+      t("simple_form.labels.defaults.mission"),
+      with: "Example mission statement"
+    )
     click_on t("buttons.new_business")
 
     expect(page).to have_content "Example Company"
